b98f67101c6280c5b06f01367c5d94decd9cbfba
whatsgoodg <rlwjddl1596@naver.com>
2024-11-05 19:37:31 +0900

    feat(Promotion): 클래스 추가
    클래스 기능
    - promotions.md의 row를 저장할 객체

---
+++ src/main/java/store/back/promotion/entity/Promotion.java
@@ -0,0 +1,39 @@
+package store.back.promotion.entity;
+
+import java.time.LocalDate;
+
+public class Promotion {
+    private final String name;
+    private final Integer buy;
+    private final Integer get;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Promotion(String name, Integer buy, Integer get, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getBuy() {
+        return buy;
+    }
+
+    public Integer getGet() {
+        return get;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+}
