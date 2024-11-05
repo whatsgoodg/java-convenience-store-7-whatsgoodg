8dd5b028f5c80d58ff2e57372687ea4d005a16a8
whatsgoodg <rlwjddl1596@naver.com>
2024-11-05 20:11:24 +0900

    feat(Promotion): from 매서드 추가 및 단위 테스팅 수행
    매서드 기능
    - 시작일과 종료일 검증
    - 필드 개수 검증
    - 객체 생성
    
    테스팅 내용
    - 객체 생성 테스트
    - 검증 테스트

--- src/main/java/store/back/promotion/entity/Promotion.java
+++ src/main/java/store/back/promotion/entity/Promotion.java
@@ -1,6 +1,7 @@
 package store.back.promotion.entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Promotion {
     private final String name;
@@ -9,12 +10,37 @@ public class Promotion {
     private final LocalDate startDate;
     private final LocalDate endDate;
 
-    public Promotion(String name, Integer buy, Integer get, LocalDate startDate, LocalDate endDate) {
+    private Promotion(String name, Integer buy, Integer get, LocalDate startDate, LocalDate endDate) {
         this.name = name;
         this.buy = buy;
         this.get = get;
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    public static Promotion from(List<String> fields) {
+        validateFieldsCount(fields);
+
+        String name = fields.get(0);
+        Integer buy = Integer.valueOf(fields.get(1));
+        Integer get = Integer.valueOf(fields.get(2));
+        LocalDate startDate = LocalDate.parse(fields.get(3));
+        LocalDate endDate = LocalDate.parse(fields.get(4));
+
+        validatePromotionPeriod(startDate, endDate);
+        return new Promotion(name, buy, get, startDate, endDate);
+    }
+
+    private static void validateFieldsCount(List<String> fields) {
+        if (fields.size() != Promotion.class.getDeclaredFields().length) {
+            throw new IllegalArgumentException("객체 생성을 위한 List의 크기가 필드의 개수와 다릅니다.");
+        }
+    }
+
+    private static void validatePromotionPeriod(LocalDate startDate, LocalDate endDate) {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException("할인의 시작일이 종료일보다 더 나중입니다.");
+        }
     }
 
     public String getName() {
---
+++ src/test/java/store/back/promotion/entity/PromotionTest.java
@@ -0,0 +1,46 @@
+package store.back.promotion.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PromotionTest {
+
+    @Test
+    @DisplayName("List<String> 타입으로부터 객체를 생성")
+    void 객체_생성_테스트() {
+        //given
+        List<String> fields = List.of("asd", "1", "1", "2024-11-01", "2024-11-01");
+        //when
+        Promotion promotion = Promotion.from(fields);
+        //then
+        assertThat(promotion.getName()).isEqualTo("asd");
+        assertThat(promotion.getBuy()).isEqualTo(1);
+        assertThat(promotion.getGet()).isEqualTo(1);
+        assertThat(promotion.getStartDate()).isEqualTo(LocalDate.parse("2024-11-01"));
+        assertThat(promotion.getEndDate()).isEqualTo(LocalDate.parse("2024-11-01"));
+    }
+
+    @Test
+    @DisplayName("List<String> 타입으로부터 객체 생성 시 field의 개수가 적을 경우 예외 발생")
+    void 객체_생성_예외_테스트1() {
+        //given
+        List<String> fields = List.of("asd", "1", "1", "2024-11-01");
+        //when
+        //then
+        assertThatThrownBy(() -> Promotion.from(fields)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("startDate가 endDate보다 더 나중일 때 예외 발생")
+    void 객체_생성_예외_테스트2() {
+        //given
+        List<String> fields = List.of("asd", "1", "1", "2024-11-02", "2024-11-01");
+        //when
+        //then
+        assertThatThrownBy(() -> Promotion.from(fields)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
