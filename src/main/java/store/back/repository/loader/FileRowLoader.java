f603b5f98fb1a80f6e3487b9f7368d0977ed1a6f
whatsgoodg <rlwjddl1596@naver.com>
2024-11-07 23:47:52 +0900

    feat(FileRowLoader): 클래스 추가 및 구현, 단위 테스팅
    클래스 기능
    - 파일을 읽어 모든 row 를 읽음
    - List<List<String>> 타입으로 반환.
    
    테스팅 내용
    - 파일을 잘 읽어오는지 테스팅

---
+++ src/main/java/store/back/repository/loader/FileRowLoader.java
@@ -0,0 +1,30 @@
+package store.back.repository.loader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileRowLoader {
+
+    static List<List<String>> loadFileToRows(String filePath) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            return readAllRows(bufferedReader);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("파일이 존재하지 않습니다.");
+        }
+    }
+
+    static List<List<String>> readAllRows(BufferedReader bufferedReader) throws IOException {
+        List<List<String>> rows = new ArrayList<>();
+        String line;
+        bufferedReader.readLine(); // 첫 줄을 날림
+        while ((line = bufferedReader.readLine()) != null) {
+            List<String> row = Arrays.stream(line.split(",")).toList();
+            rows.add(row);
+        }
+        return rows;
+    }
+}
