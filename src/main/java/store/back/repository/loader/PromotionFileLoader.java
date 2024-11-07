8a57f56a0b1545bbb1b5522443692746edd2bb7b
whatsgoodg <rlwjddl1596@naver.com>
2024-11-07 23:57:36 +0900

    feat(PromotionFileLoader): 클래스 추가 및 구현, 단위 테스팅
    클래스 기능
    - 파일을 읽어서 List<Promotion> 객체로 반환
    
    테스팅 내용
    - 매서드를 사용하여 List<Promotion> 객체 반환.

---
+++ src/main/java/store/back/repository/loader/PromotionFileLoader.java
@@ -0,0 +1,29 @@
+package store.back.repository.loader;
+
+import java.time.LocalDate;
+import java.util.List;
+import store.back.domain.Promotion;
+
+public class PromotionFileLoader {
+    private static final String promotionFilePath = "src/main/resources/promotions.md";
+
+    public static List<Promotion> loadPromotions(){
+        List<List<String>> rows = FileRowLoader.loadFileToRows(promotionFilePath);
+
+        return rows.stream().map(PromotionFileLoader::mapRowToPromotion).toList();
+    }
+
+    private static Promotion mapRowToPromotion(List<String> rows){
+        if(rows.size() != Promotion.class.getDeclaredFields().length){
+            throw new IllegalArgumentException("Promotion의 필드 개수와 column 개수가 동일하지 않습니다.");
+        }
+        String name = rows.get(0);
+        Integer buy = Integer.valueOf(rows.get(1));
+        Integer get = Integer.valueOf(rows.get(2));
+        LocalDate startDate = LocalDate.parse(rows.get(3));
+        LocalDate endDate = LocalDate.parse(rows.get(4));
+
+        return new Promotion(name, buy, get, startDate, endDate);
+    }
+
+}
---
+++ src/test/java/store/back/repository/loader/PromotionFileLoaderTest.java
@@ -0,0 +1,25 @@
+package store.back.repository.loader;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.back.domain.Promotion;
+
+class PromotionFileLoaderTest {
+
+    @Test
+    @DisplayName("promotions.md 파일을 읽어 List<Promotion>으로 반환 테스트")
+    void 파일에서_객체생성_테스트(){
+        //given
+        Promotion promotion1 = new Promotion("탄산2+1", 2, 1, LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-31"));
+        Promotion promotion2 = new Promotion("null", 1, 1, LocalDate.parse("2024-01-01"), LocalDate.parse("2024-01-01"));
+        //when
+        List<Promotion> promotions = PromotionFileLoader.loadPromotions();
+        //then
+        assertThat(promotions.size()).isEqualTo(4);
+        assertThat(promotions).usingRecursiveFieldByFieldElementComparator().contains(promotion1, promotion2);
+    }
+}
