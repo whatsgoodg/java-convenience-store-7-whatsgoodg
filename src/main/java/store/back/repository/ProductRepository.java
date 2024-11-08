d633fc08361060d8b6c2ef3ba9c8605abfe8d8ed
whatsgoodg <rlwjddl1596@naver.com>
2024-11-08 16:38:22 +0900

    feat(ProductRepository): 클래스 추가 및 구현, 단위 테스팅
    클래스 기능
    - FileLoader 클래스로부터 Product를 로드해와 Map에 저장
    - ID 기반으로 Map에 저장, Integer 타입의 ID 사용
    
    테스팅 내용
    - 모든 Product 반환
    - 이름을 통한 Product들 반환
    - ID를 통한 Product 반환
    - Product 저장

---
+++ src/main/java/store/back/repository/ProductRepository.java
@@ -0,0 +1,45 @@
+package store.back.repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import store.back.domain.Product;
+import store.back.repository.loader.ProductFileLoader;
+
+public class ProductRepository {
+    private final Map<Integer, Product> products;
+
+    public ProductRepository() {
+        List<Product> products = ProductFileLoader.loadProducts();
+        this.products = products.stream().collect(Collectors.toMap(Product::getId, product -> product));
+    }
+
+    public List<Product> findAll() {
+        return products.entrySet().stream().sorted(Map.Entry.comparingByKey()).map(Map.Entry::getValue).toList();
+    }
+
+    public List<Product> findByName(String name) {
+        List<Product> findProducts = findAll();
+        return findProducts.stream().filter(product -> Objects.equals(product.getName(), name)).toList();
+    }
+
+    public Product save(Product product) {
+        if (!products.containsKey(product.getId())) {
+            throw new IllegalArgumentException("해당 키를 가진 객체가 존재하지 않습니다.");
+        }
+        products.put(product.getId(), product);
+
+        return products.get(product.getId());
+    }
+
+    public Optional<Product> findById(Integer id) {
+        Product product = products.get(id);
+        if (product == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(product);
+    }
+}
