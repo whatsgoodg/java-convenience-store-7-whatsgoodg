47ca6671316199c2d4cdc9671e6e153774088546
whatsgoodg <rlwjddl1596@naver.com>
2024-11-10 04:16:43 +0900

    feat(ExceptionMessage): 예외 메시지 추가

--- src/main/java/store/global/enums/ExceptionMessage.java
+++ src/main/java/store/global/enums/ExceptionMessage.java
@@ -3,7 +3,9 @@ package store.global.enums;
 public enum ExceptionMessage {
     OUT_OF_STOCK("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
     QUANTITY_BELOW_ZERO("[ERROR] 구매하려는 상품의 개수는 1개 이상이여야 합니다. 다시 입력해 주세요."),
-    PRODUCT_NAME_NOT_FOUND("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+    PRODUCT_NAME_NOT_FOUND("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."),
+    INVALID_PRODUCT_INPUT("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    INVALID_YN_INPUT("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
 
     private final String message;
 
