c38d85d935899559e82c9a99a99ff0de5d6690df
whatsgoodg <rlwjddl1596@naver.com>
2024-11-10 04:13:59 +0900

    feat(ProductInfo): POJO 객체 추가
    해당 레코드 기능
    - 상품 이름, 가격, 수량, 프로모션 이름을 저장

---
+++ src/main/java/store/global/dto/response/display/ProductInfo.java
@@ -0,0 +1,14 @@
+package store.global.dto.response.display;
+
+import store.back.domain.Product;
+
+public record ProductInfo(String name, Integer price, Integer quantity, String promotionName) {
+
+    public static ProductInfo from(Product product) {
+        return new ProductInfo(
+                product.getName(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getPromotion().getName());
+    }
+}
