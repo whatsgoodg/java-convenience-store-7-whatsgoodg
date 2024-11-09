a400cfe4947e751500d0fe48e155a9a3c3d4b223
whatsgoodg <rlwjddl1596@naver.com>
2024-11-09 20:58:54 +0900

    feat(PurchaseProductInfo): 클래스 추가, 기능 구현 및 단위 테스팅
    클래스 기능
    - 사용자가 구매할 상품 정보(이름. 수량)을 저장할 DTO
    
    테스팅 내용
    - 사용자가 수량을 1미만으로 입력하면 예외 발생

---
+++ src/main/java/store/global/dto/request/PurchaseProductInfo.java
@@ -0,0 +1,17 @@
+package store.global.dto.request;
+
+import store.global.exception.InvalidPurchaseQuantityException;
+
+public record PurchaseProductInfo(String name, Integer quantity) {
+    public PurchaseProductInfo(String name, Integer quantity) {
+        validate(quantity);
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    private void validate(Integer quantity) {
+        if (quantity < 1) {
+            throw new InvalidPurchaseQuantityException();
+        }
+    }
+}
