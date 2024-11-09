6ad019fb51e3fd12baacde2b1b35b9b2d692bb63
whatsgoodg <rlwjddl1596@naver.com>
2024-11-09 21:07:50 +0900

    feat(StockValidationService): 클래스 추가, 기능 구현 및 단위 테스팅
    클래스 기능
    - 구매할 정보를 받고, 상품이 존재하는지 수량이 넘치지 않는지 검증하는 서비스
    테스팅 내용
    - 상품이 존재하지 않을 때 예외 테스트
    - 구매할 수량이 기존의 수량보다 더 많을 때 예외 테스트
    - 상품이 존재하고 수량이 넉넉할 때 아무 예외를 반환하지 않는 테스트

---
+++ src/main/java/store/back/service/product/StockValidationService.java
@@ -0,0 +1,33 @@
+package store.back.service.product;
+
+import java.util.List;
+import store.back.domain.Product;
+import store.global.dto.request.PurchaseProductInfo;
+import store.global.exception.InvalidProductNameException;
+import store.global.exception.OutOfStockException;
+
+public class StockValidationService {
+    private final ProductQueryService productQueryService = new ProductQueryService();
+
+    public void validate(List<PurchaseProductInfo> purchaseProductInfos) {
+        purchaseProductInfos.forEach(this::validateProductName);
+        purchaseProductInfos.forEach(this::validatePurchasable);
+    }
+
+    private void validateProductName(PurchaseProductInfo purchaseProductInfo) {
+        List<Product> products = productQueryService.findByName(purchaseProductInfo.name());
+
+        if (products.isEmpty()) {
+            throw new InvalidProductNameException();
+        }
+    }
+
+    private void validatePurchasable(PurchaseProductInfo PurchaseProductInfo) {
+        List<Product> products = productQueryService.findByName(PurchaseProductInfo.name());
+
+        int totalQuantity = products.stream().mapToInt(Product::getQuantity).sum();
+        if (totalQuantity < PurchaseProductInfo.quantity()) {
+            throw new OutOfStockException();
+        }
+    }
+}
---
+++ src/test/java/store/back/service/product/StockValidationServiceTest.java
@@ -0,0 +1,47 @@
+package store.back.service.product;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.global.dto.request.PurchaseProductInfo;
+import store.global.exception.InvalidProductNameException;
+import store.global.exception.OutOfStockException;
+
+class StockValidationServiceTest {
+
+    private final StockValidationService stockValidationService = new StockValidationService();
+
+    @Test
+    @DisplayName("구매할 상품이 존재하지 않을 때 예외 발생")
+    void 구매상품_존재하지않음_예외_테스트() {
+        //given
+        List<PurchaseProductInfo> purchaseProductInfos = List.of(new PurchaseProductInfo("햄버거", 12));
+        //when
+        assertThatThrownBy(() -> stockValidationService.validate(purchaseProductInfos)).isInstanceOf(
+                InvalidProductNameException.class);
+    }
+
+    @Test
+    @DisplayName("구매할 상품의 수량을 넘었을 때 예외 발생")
+    void 구매수량_초과_예외_테스트() {
+        //given
+        List<PurchaseProductInfo> purchaseProductInfos = List.of(new PurchaseProductInfo("콜라", 22));
+        //when
+        assertThatThrownBy(() -> stockValidationService.validate(purchaseProductInfos)).isInstanceOf(
+                OutOfStockException.class);
+    }
+
+    @Test
+    @DisplayName("구매할 상품이 존재하고 수량이 적합할 때 아무일도 발생하지 않음")
+    void 구매상품_존재_수량_존재_테스트() {
+        //given
+        List<PurchaseProductInfo> purchaseProductInfos = List.of(
+                new PurchaseProductInfo("콜라", 5),
+                new PurchaseProductInfo("물", 2));
+        //when
+        assertThatCode(() -> stockValidationService.validate(purchaseProductInfos)).doesNotThrowAnyException();
+    }
+}
