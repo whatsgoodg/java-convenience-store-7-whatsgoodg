acd4aad4b4e0b9d27579adbdf4ba1af4d9339919
whatsgoodg <rlwjddl1596@naver.com>
2024-11-10 04:16:03 +0900

    feat(ProductDisplayFacade): 서비스 구현 및 테스팅
    해당 서비스 기능
    - 모든 Product의 정보를 ProductDisplayResponseDTO로 반환
    
    테스팅 내용
    - 모든 Product 정보를 반환하는지 테스팅

---
+++ src/test/java/store/back/facade/product/ProductDisplayFacadeTest.java
@@ -0,0 +1,26 @@
+package store.back.facade.product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.global.dto.response.display.ProductDisplayResponseDTO;
+import store.global.dto.response.display.ProductInfo;
+
+class ProductDisplayFacadeTest {
+    private final ProductDisplayFacade productDisplayFacade = new ProductDisplayFacade();
+
+    @Test
+    @DisplayName("모든 상품 정보를 반환하는 테스트")
+    void 모든_상품_정보_반환() {
+        //given
+        ProductInfo productInfo = new ProductInfo("콜라", 1000, 10, "탄산2+1");
+        //when
+        ProductDisplayResponseDTO productDisplayResponseDTO = productDisplayFacade.getAllProducts();
+        List<ProductInfo> productInfos = productDisplayResponseDTO.productInfos();
+        //then
+        assertThat(productInfos.size()).isEqualTo(16);
+        assertThat(productInfos).usingRecursiveFieldByFieldElementComparator().contains(productInfo);
+    }
+}
