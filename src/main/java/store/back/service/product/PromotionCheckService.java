7c6b61f00172c6bc18ca66592ebc735b3c9f00f3
whatsgoodg <rlwjddl1596@naver.com>
2024-11-09 21:20:03 +0900

    feat(PromotionCheckService): 서비스 클래스 구현 및 단위 테스팅
    서비스의 기능
    - 구매하고자 하는 상품에 대해 추가로 증정받을 수 있는 상품 정보(이름, 수량)를 리스트로 반환
    - 그러한 상품이 없으면 빈 리스트를 반환
    
    테스팅 내용
    - 증정 가능한 상품 정보 반환
    - 그러한 상품이 없을 때 빈 리스트 반환

---
+++ src/main/java/store/back/service/product/PromotionCheckService.java
@@ -0,0 +1,50 @@
+package store.back.service.product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import store.back.domain.Product;
+import store.global.dto.request.PurchaseProductInfo;
+import store.global.dto.response.PromotionalProductInfo;
+
+public class PromotionCheckService {
+    private final ProductQueryService productQueryService = new ProductQueryService();
+
+    /**
+     * 모든 PurchaseProductInfos 구매하고자하는 상품에 대해 추가로 증정받을 수 있는 상품 정보(상품 이름, 수량)들을 반환
+     *
+     * @param purchaseProductInfos 구매하고자 하는 상품 정보
+     * @return 추가로 증정받을 수 있는 상품 정보를 List로 반환, 빈 리스트 반환 시 그러한 상품이 존재하지 않음
+     */
+    public List<PromotionalProductInfo> getPromotionalProductInfos(List<PurchaseProductInfo> purchaseProductInfos) {
+        List<PromotionalProductInfo> promotionalProductInfos = new ArrayList<>();
+        purchaseProductInfos.forEach(purchaseProductInfo -> {
+            Optional<PromotionalProductInfo> promotionalProductInfo = getPromotionalProductInfo(purchaseProductInfo);
+            promotionalProductInfo.ifPresent(promotionalProductInfos::add);
+        });
+        return promotionalProductInfos.stream().toList();
+    }
+
+    private Optional<PromotionalProductInfo> getPromotionalProductInfo(PurchaseProductInfo purchaseProductInfo) {
+        Optional<Product> productWithPromotion = productQueryService.findProductWithPromotion(
+                purchaseProductInfo.name());
+        // 프로모션이 존재하는 상품이 없으면 증정받을 수 있는 것이 없음
+        if (productWithPromotion.isEmpty()) {
+            return Optional.empty();
+        }
+        Product product = productWithPromotion.get();
+        return calculatePromotionalProductInfo(product, purchaseProductInfo.quantity());
+    }
+
+    private Optional<PromotionalProductInfo> calculatePromotionalProductInfo(Product product, Integer quantity) {
+        Integer buy = product.getPromotion().getBuy();
+        Integer get = product.getPromotion().getGet();
+        int buyAfterPromotion = quantity % (buy + get);
+        int addedQuantity = quantity + get;
+        // 프로모션 적용 후 구매한 수량이 buy와 같아야함, 그리고 get을 추가로 받았을 때의 수량이 상품 재고보다 작거나 같아야함.
+        if (buyAfterPromotion == buy && addedQuantity <= product.getQuantity()) {
+            return Optional.of(new PromotionalProductInfo(product.getName(), get));
+        }
+        return Optional.empty();
+    }
+}
