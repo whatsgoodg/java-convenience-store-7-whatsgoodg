c02f250beb63bb78f67a794fef5f11adeaa84e5f
whatsgoodg <rlwjddl1596@naver.com>
2024-11-10 18:31:41 +0900

    feat(NonPromotionalProductMessageBuilder): 해당 클래스 기능 변경
    변경 내용
    - List<NonPromotionalProductInfo> 가 아닌 하나의 객체에 대해 입력 메시지 빌드
    - 이에 따른 테스팅 수행

--- src/test/java/store/front/builder/input/NonPromotionalProductMessageBuilderTest.java
+++ src/test/java/store/front/builder/input/NonPromotionalProductMessageBuilderTest.java
@@ -2,7 +2,6 @@ package store.front.builder.input;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import store.global.dto.response.purchase.NonPromotionalProductInfo;
@@ -10,14 +9,13 @@ import store.global.dto.response.purchase.NonPromotionalProductInfo;
 class NonPromotionalProductMessageBuilderTest {
 
     @Test
-    @DisplayName("프로모션이 적용되는 상품에 대해 입력 메시지 생성 테스트")
+    @DisplayName("정가로 구매해야하는 상품에 대해 입력 메시지 생성 테스트")
     void 정가_구매_상품_입력_메시지_생성() {
         //given
-        List<NonPromotionalProductInfo> nonPromotionalProductInfos = List.of(new NonPromotionalProductInfo("콜라", 1),
-                new NonPromotionalProductInfo("사이다", 2));
+        NonPromotionalProductInfo nonPromotionalProductInfos = new NonPromotionalProductInfo("사이다", 2);
         //when
         String builtMessage = NonPromotionalProductMessageBuilder.build(nonPromotionalProductInfos);
         //then
-        assertThat(builtMessage).contains("현재 콜라,사이다 1,2개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+        assertThat(builtMessage).contains("현재 사이다 2개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
     }
 }
