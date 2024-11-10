a2abafc65606f4cd2122aa1e72c4b202f32d0bd3
whatsgoodg <rlwjddl1596@naver.com>
2024-11-10 18:06:53 +0900

    feat(ExceptionMessage): 예외 메시지 추가
    추가된 메시지
    - 사용자가 프로모션 할인이 적용되지 않는 상품에 대해 구매를 원치 않았을 때 구매 상품 목록이 모두 비어버리는 상황을 예외로 처리하기 위한 메시지

--- src/main/java/store/global/enums/ExceptionMessage.java
+++ src/main/java/store/global/enums/ExceptionMessage.java
@@ -6,7 +6,9 @@ public enum ExceptionMessage {
     PRODUCT_NAME_NOT_FOUND("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."),
     INVALID_PRODUCT_INPUT("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     INVALID_YN_INPUT("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요."),
-    PRODUCT_NOT_FOUND_CART("상품이 장바구니에 존재하지 않습니다.");
+
+    PRODUCT_NOT_FOUND_CART("상품이 장바구니에 존재하지 않습니다."),
+    NO_PURCHASE_PRODUCT_FOUND("구매하고자 하는 상품이 존재하지 않습니다. 다시 구매를 수행합니다.");
 
     private final String message;
 
