63601618cd16921b8230352bbd03678037e6e5d2
whatsgoodg <rlwjddl1596@naver.com>
2024-11-10 17:10:21 +0900

    feat(InvalidProductNameException): 매개변수 생성자 추가

--- src/main/java/store/global/exception/InvalidProductNameException.java
+++ src/main/java/store/global/exception/InvalidProductNameException.java
@@ -2,8 +2,12 @@ package store.global.exception;
 
 import store.global.enums.ExceptionMessage;
 
-public class InvalidProductNameException extends IllegalArgumentException{
+public class InvalidProductNameException extends IllegalArgumentException {
     public InvalidProductNameException() {
         super(ExceptionMessage.PRODUCT_NAME_NOT_FOUND.getMessage());
+    }
+
+    public InvalidProductNameException(String message) {
+        super(message);
     }
 }
