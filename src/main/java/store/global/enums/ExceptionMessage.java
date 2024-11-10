809866d7e4a401c8d34e598fc76ac82fe5cd1153
whatsgoodg <rlwjddl1596@naver.com>
2024-11-10 17:09:59 +0900

    feat(ExceptionMessage): 예외 메시지 추가
    메시지 기능
    - 장바구니에 존재하지 않는 상품을 검색했을 때 발생할 예외

--- src/main/java/store/global/enums/ExceptionMessage.java
+++ src/main/java/store/global/enums/ExceptionMessage.java
@@ -5,7 +5,8 @@ public enum ExceptionMessage {
     QUANTITY_BELOW_ZERO("[ERROR] 구매하려는 상품의 개수는 1개 이상이여야 합니다. 다시 입력해 주세요."),
     PRODUCT_NAME_NOT_FOUND("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."),
     INVALID_PRODUCT_INPUT("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
-    INVALID_YN_INPUT("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+    INVALID_YN_INPUT("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요."),
+    PRODUCT_NOT_FOUND_CART("상품이 장바구니에 존재하지 않습니다.");
 
     private final String message;
 
