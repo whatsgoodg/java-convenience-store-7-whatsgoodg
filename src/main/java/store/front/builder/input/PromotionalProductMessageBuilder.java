7c9ddb814c8db909baffb855b663f7650f07503b
whatsgoodg <rlwjddl1596@naver.com>
2024-11-10 18:31:24 +0900

    feat(PromotionalProductMessageBuilder): 해당 클래스 기능 변경
    변경 내용
    - List<PromotionalProductInfo> 가 아닌 하나의 객체에 대해 입력 메시지 빌드

--- src/main/java/store/front/builder/input/PromotionalProductMessageBuilder.java
+++ src/main/java/store/front/builder/input/PromotionalProductMessageBuilder.java
@@ -1,22 +1,12 @@
 package store.front.builder.input;
 
-import java.util.List;
 import store.global.dto.response.purchase.PromotionalProductInfo;
 import store.global.enums.InputMessage;
 
 public class PromotionalProductMessageBuilder {
-    private static final String COMMA = ",";
 
-    public static String build(List<PromotionalProductInfo> promotionalProductInfos) {
-        StringBuilder productNameBuilder = new StringBuilder();
-        StringBuilder productQuantityBuilder = new StringBuilder();
-
-        promotionalProductInfos.forEach(promotionalProductInfo -> {
-            productNameBuilder.append(promotionalProductInfo.name()).append(COMMA);
-            productQuantityBuilder.append(promotionalProductInfo.quantity()).append(COMMA);
-        });
-        String builtProductName = productNameBuilder.substring(0, productNameBuilder.length() - 1);
-        String builtProductQuantity = productQuantityBuilder.substring(0, productQuantityBuilder.length() - 1);
-        return String.format(InputMessage.PROMOTION.getMessage(), builtProductName, builtProductQuantity);
+    public static String build(PromotionalProductInfo promotionalProductInfo) {
+        return String.format(InputMessage.PROMOTION.getMessage(), promotionalProductInfo.name(),
+                promotionalProductInfo.quantity());
     }
 }
--- src/test/java/store/front/builder/input/PromotionalProductMessageBuilderTest.java
+++ src/test/java/store/front/builder/input/PromotionalProductMessageBuilderTest.java
@@ -2,7 +2,6 @@ package store.front.builder.input;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import store.global.dto.response.purchase.PromotionalProductInfo;
@@ -10,14 +9,13 @@ import store.global.dto.response.purchase.PromotionalProductInfo;
 class PromotionalProductMessageBuilderTest {
 
     @Test
-    @DisplayName("정가로 구매해야하는 상품에 대해 입력 메시지 생성 테스트")
-    void 정가_구매_상품_입력_메시지_생성() {
+    @DisplayName("프로모션이 적용되는 상품에 대해 입력 메시지 생성 테스트")
+    void 프로모션_증정_상품_입력_메시지_생성() {
         //given
-        List<PromotionalProductInfo> promotionalProductInfos = List.of(new PromotionalProductInfo("콜라", 1),
-                new PromotionalProductInfo("사이다", 2));
+        PromotionalProductInfo promotionalProductInfo = new PromotionalProductInfo("콜라", 1);
         //when
-        String builtMessage = PromotionalProductMessageBuilder.build(promotionalProductInfos);
+        String builtMessage = PromotionalProductMessageBuilder.build(promotionalProductInfo);
         //then
-        assertThat(builtMessage).contains("현재 콜라,사이다은(는) 1,2개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        assertThat(builtMessage).contains("현재 콜라은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
     }
 }
