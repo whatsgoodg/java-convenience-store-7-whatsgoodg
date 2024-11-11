846c551de1ba77bea32173d8a3655379d4df1425
whatsgoodg <rlwjddl1596@naver.com>
2024-11-11 09:07:46 +0900

    feat(Application): 컨트롤러 호출부 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,10 @@
 package store;
 
+import store.back.controller.ConvenienceStoreController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConvenienceStoreController convenienceStoreController = new ConvenienceStoreController();
+        convenienceStoreController.run();
     }
 }
